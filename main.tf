5b90fc3ba53999e85546eb760ae5c4d35635c2ac
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-05-05 23:26:40 -0300

    update main.tf

--- main.tf
+++ main.tf
@@ -28,9 +28,7 @@ resource "aws_db_instance" "mysql" {
   engine_version    = "8.0"
   instance_class    = "db.t3.micro"
   allocated_storage = 20
-  db_name           = var.db_name
-  username          = var.db_user
-  password          = var.db_password
+  db_name           = "mysql-db"
   #  db_subnet_group_name   = aws_db_subnet_group.default.name
   #  vpc_security_group_ids = [aws_security_group.rds_sg.id]
   skip_final_snapshot = true

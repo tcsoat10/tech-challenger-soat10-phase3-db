8a7de5f89b3d3890a0514c2cc4d8820a4b4a9a33
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-05-05 23:24:53 -0300

    update main.tf

--- main.tf
+++ main.tf
@@ -31,8 +31,8 @@ resource "aws_db_instance" "mysql" {
   db_name                = var.db_name
   username               = var.db_user
   password               = var.db_password
-  db_subnet_group_name   = aws_db_subnet_group.default.name
-  vpc_security_group_ids = [aws_security_group.rds_sg.id]
+#  db_subnet_group_name   = aws_db_subnet_group.default.name
+#  vpc_security_group_ids = [aws_security_group.rds_sg.id]
   skip_final_snapshot    = true
   publicly_accessible    = true
 }

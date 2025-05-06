c9ae2376948c501184b80264088e35641fde9fbc
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-05-05 23:23:41 -0300

    update main.tf

--- main.tf
+++ main.tf
@@ -18,27 +18,8 @@ terraform {
   }
 }
 
-variable "AWS_ACCESS_KEY_ID" {
-  description = "AWS Access Key ID"
-  type        = string
-}
-
-variable "AWS_SECRET_ACCESS_KEY" {
-  description = "AWS Secret Access Key"
-  type        = string
-}
-
-variable "AWS_SESSION_TOKEN" {
-  description = "AWS Session Token (se estiver usando credenciais temporárias)"
-  type        = string
-  default     = ""
-}
-
 provider "aws" {
-  region        = "us-east-1"
-  access_key    = var.AWS_ACCESS_KEY_ID
-  secret_key    = var.AWS_SECRET_ACCESS_KEY
-  session_token = var.AWS_SESSION_TOKEN
+  region = "us-east-1"
 }
 
 resource "aws_db_instance" "mysql" {

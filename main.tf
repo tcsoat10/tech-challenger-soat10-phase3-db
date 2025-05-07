b091acbf396c03d8c33631cc0be50d1a72382aa0
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-05-06 22:25:28 -0300

    add variables and gitignore

--- main.tf
+++ main.tf
@@ -33,6 +33,8 @@ resource "aws_db_instance" "mysql" {
   instance_class    = "db.t3.micro"
   allocated_storage = 20
   db_name           = "mysql-db"
+  username          = var.db_username
+  password          = var.db_password
   #  db_subnet_group_name   = aws_db_subnet_group.default.name
   #  vpc_security_group_ids = [aws_security_group.rds_sg.id]
   skip_final_snapshot = true

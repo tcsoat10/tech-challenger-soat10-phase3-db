288b3a4b0c781f94db890ceeb5f9b2d1b71ea750
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-05-07 21:20:21 -0300

    terraform fmt

--- main.tf
+++ main.tf
@@ -27,14 +27,14 @@ resource "aws_vpc" "example" {
 }
 
 resource "aws_db_instance" "mysql" {
-  identifier        = "mysql-db"
-  engine            = "mysql"
-  engine_version    = "8.0"
-  instance_class    = "db.t3.micro"
-  allocated_storage = 20
-  db_name           = "mysql_db"
-  username          = var.db_username
-  password          = var.db_password
+  identifier          = "mysql-db"
+  engine              = "mysql"
+  engine_version      = "8.0"
+  instance_class      = "db.t3.micro"
+  allocated_storage   = 20
+  db_name             = "mysql_db"
+  username            = var.db_username
+  password            = var.db_password
   skip_final_snapshot = true
   publicly_accessible = true
 }

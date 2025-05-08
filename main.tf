fbd271d3c968c5250fa5d6dd29e81106ecc96285
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-05-07 21:19:30 -0300

    code cleanup

--- main.tf
+++ main.tf
@@ -35,8 +35,6 @@ resource "aws_db_instance" "mysql" {
   db_name           = "mysql_db"
   username          = var.db_username
   password          = var.db_password
-  #  db_subnet_group_name   = aws_db_subnet_group.default.name
-  #  vpc_security_group_ids = [aws_security_group.rds_sg.id]
   skip_final_snapshot = true
   publicly_accessible = true
 }

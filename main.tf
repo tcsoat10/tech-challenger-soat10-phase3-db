f842a8863e20e3cdf90cd5f5d71e765fe06d1e2f
LucasRamos7 <86800737+LucasRamos7@users.noreply.github.com>
2025-05-10 18:24:00 -0300

    change db name

--- main.tf
+++ main.tf
@@ -32,7 +32,7 @@ resource "aws_db_instance" "mysql" {
   engine_version      = "8.0"
   instance_class      = "db.t3.micro"
   allocated_storage   = 20
-  db_name             = "mysql_db"
+  db_name             = "mysql_db_pr_test"
   username            = var.db_username
   password            = var.db_password
   skip_final_snapshot = true
